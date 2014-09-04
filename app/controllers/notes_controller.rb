3afc9bb1ba1cb1f8061fcdf318e05d6c51784404
Vadim Lazebny <vadim.lazebny@gmail.com>
2014-09-04 15:10:33 +0300

    Add: right column to notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,11 @@
 class NotesController < ApplicationController
   def index
     @notes = Note.order(updated_at: :desc).page(params[:page]).per(10)
+
+    @posts = Post.order(updated_at: :desc).page(params[:page]).per(3)
+    @menu_posts = Post.order(updated_at: :desc).first(3)
+    @quote = Quote.day_qoute
+    @galeries = Galery.includes(:images).all
+    @note = Note.last
   end
 end
