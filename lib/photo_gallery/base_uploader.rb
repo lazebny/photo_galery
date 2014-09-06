80fe7b2b88cc864db5f72ee83eedec24dd1e76b2
Vadim Lazebny <vadim.lazebny@gmail.com>
2014-09-06 19:12:17 +0300

    Fix: add

--- lib/photo_gallery/base_uploader.rb
+++ lib/photo_gallery/base_uploader.rb
@@ -4,11 +4,12 @@ class PhotoGallery::BaseUploader < CarrierWave::Uploader::Base
   # google_password 'Woodstok69'
 
   # def url
-  #   if Rails.env.production?
-  #     'https://dl.dropboxusercontent.com/1/view/cph48ze9nckmgb4/Приложения/photo_galery_store/uploads/'
-  #   else
-  #     super
-  #   end
+    # binding.pry
+    # if Rails.env.production?
+    #   'https://dl.dropboxusercontent.com/1/view/cph48ze9nckmgb4/Приложения/photo_galery_store/uploads/'
+    # else
+    #   'https://dl.dropboxusercontent.com/1/view/cph48ze9nckmgb4/Приложения/photo_galery_store/' + path if path
+    # end
   # end
 
   def default_url
