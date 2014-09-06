591517d4e044b0c1249b6ee2a35c24feab6b5769
Vadim Lazebny <vadim.lazebny@gmail.com>
2014-09-06 18:27:31 +0300

    Add: kaminari config. Default photo

--- lib/photo_gallery/base_uploader.rb
+++ lib/photo_gallery/base_uploader.rb
@@ -3,13 +3,25 @@ class PhotoGallery::BaseUploader < CarrierWave::Uploader::Base
   # google_login    'vadim.lazebny@gmail.com'
   # google_password 'Woodstok69'
 
+  # def url
+  #   if Rails.env.production?
+  #     'https://dl.dropboxusercontent.com/1/view/cph48ze9nckmgb4/Приложения/photo_galery_store/uploads/'
+  #   else
+  #     super
+  #   end
+  # end
+
+  def default_url
+    fallback = "fallback/" + ["no-image.png"].compact.join('_')
+    ActionController::Base.helpers.asset_path fallback
+  end
+
   if Rails.env.production?
     storage :dropbox
   else
     # storage :google_drive
     # storage :dropbox
     storage :file
-    # storage :file
   end
   # storage :fog
 end
