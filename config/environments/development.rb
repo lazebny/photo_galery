a9669acb89292546b938bccc715775bbe9f588e0
Vika <vika030718@gmail.com>
2014-09-07 17:40:37 +0100

    Fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,5 @@ PhotoGallery::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.log_level = :warn
+  config.log_level = :info
 end
